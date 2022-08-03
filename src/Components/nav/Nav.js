a32a0d6b8c2a76e3f6275e9b1d54a877281a29c9
Rio Foster <friotious@gmail.com>
2022-08-03 15:18:03 -0700

    Add router Link

--- src/Components/nav/Nav.js
+++ src/Components/nav/Nav.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './Nav.css'
+import { Link } from "react-router-dom";
 
 const Nav = () => {
     return (
-        <div className='Nav'></div>
+        <div className='Nav' >
+            <Link to='/' className='app-name' style={{ textDecoration: "none" }}> 
+                <h1>NYT Reader</h1>
+            </Link>
+        </div>
     );
 }
 
