e6e5342e3ef13f76443268493638455a1ed71794
Rio Foster <friotious@gmail.com>
2022-08-03 15:17:30 -0700

    Add url link path

--- src/Components/articleListContainer/ArticleListContainer.js
+++ src/Components/articleListContainer/ArticleListContainer.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import ListArticle from '../listArticle/ListArticle';
+import { Link } from "react-router-dom";
 import './ArticleListContainer.css'
 
-const ArticleListContainer = () => {
+const ArticleListContainer = ({articles, section}) => {
+
+
+    const articleList = articles.map((article, index) => {
+        return  <Link to={`/article-detail/${article.title}`} id={article.title} key={index} style={{ textDecoration: "none" }}>
+                    <ListArticle article={article} key={index} />
+                </Link>
+    })
     return (
-        <div className='ArticleListContainer'>
-            section list
-        </div>
+        <>
+            <div className='section-title'>{section}</div>
+            <div className='list-container'>
+                {articleList}
+            </div>
+        </>
     );
 }
 
