05e50f37c5b396dedc2dde5e06c24255b3e9d5d7
Rio Foster <friotious@gmail.com>
2022-08-03 15:15:40 -0700

    Update Nav link paths, add styling

--- src/Components/listArticle/ListArticle.js
+++ src/Components/listArticle/ListArticle.js
@@ -1,9 +1,25 @@
 import React from 'react';
-import './ListArticle'
+import './ListArticle.css'
 
-const ListArticle = () => {
-    return (
-        <div className='ListArticle'></div>
+const ListArticle = ({article}) => {
+   const checkForImage = () =>  {
+    if (article.multimedia === null) {
+        return <p>no image available</p>
+    } else {
+        return <img className='la-img' src={article.multimedia[1].url}></img>
+    }
+} 
+
+    
+
+    
+ return (
+        <div className='list-article'>
+            <p className='la-title'>{article.title}</p>
+            <p className='la-abstract'>{article.abstract}</p>
+            <p className='la-byline' >{article.byline}</p>
+            {checkForImage()}
+        </div>
     );
 }
 
